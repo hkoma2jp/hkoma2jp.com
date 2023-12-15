cd6ccbe1c0409ad33ca76c0053f84e93e8deedbf
hkoma2jp <h.koma2.jp@gmail.com>
2023-12-16 08:35:35 +0900

    use remark-gfm

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,7 +1,8 @@
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
-import remark from 'remark'
+import { remark } from 'remark'
+import remarkGfm from 'remark-gfm';
 import html from 'remark-html'
 
 const postsDirectory = path.join(process.cwd(), 'posts')
@@ -56,6 +57,7 @@ export async function getPostData(id: string) {
 
   // Use remark to convert markdown into HTML string
   const processedContent = await remark()
+    .use(remarkGfm)
     .use(html)
     .process(matterResult.content)
   const contentHtml = processedContent.toString()
